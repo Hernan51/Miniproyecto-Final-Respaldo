f3bb3ce50a593f530046305216c8d6748dc5a32a
betocampost <106752128+betocampost@users.noreply.github.com>
2023-06-16 19:42:47 -0800

    Login y logout

---
+++ Login-logoutV1/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { routing } from "./app.routing";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, routing, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
